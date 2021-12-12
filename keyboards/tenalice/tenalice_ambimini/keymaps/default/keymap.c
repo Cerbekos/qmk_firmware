2e4cbe3f57608268dc68913a2af26ced8188c302
Cerbekos <t.naito1818@gmail.com>
2021-12-13 00:06:38 +0900

    add new file

---
+++ keyboards/tenalice/tenalice_ambimini/keymaps/default/keymap.c
@@ -0,0 +1,68 @@
+/*
+Copyright 2021 Salicylic_Acid
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "keymap_jp.h"
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layer_number {
+  _QWERTY = 0,
+  _FN,
+  _FN2
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT(
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,RGUI_T(KC_UP),LCTL_T(KC_RIGHT),KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,LALT_T(KC_LEFT), LSFT_T(KC_DOWN),LT(1,KC_ENT), KC_H, KC_J,  KC_K,  KC_L, KC_MINS,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        LT(2,KC_SPC),          KC_B,    KC_N,    KC_M, KC_COMM,RSFT_T(KC_DOT)
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+  ),
+  [_FN] = LAYOUT(
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+        KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_PGUP,  KC_END,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+        KC_CAPS, _______, _______, _______, _______, KC_HOME, KC_PGDN, _______, _______, _______, _______, _______, _______,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+        C(KC_Z), C(KC_X), C(KC_C), C(KC_V),  KC_EQL,            TG(3),          RGB_TOG, RGB_MOD,RGB_RMOD, RGB_HUI, RGB_HUD
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+  ),
+  [_FN2] = LAYOUT(
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+        KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  KC_ESC,  KC_DEL,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+        _______, _______, _______, _______, _______,  KC_TAB, KC_BSPC,   TG(3), _______, _______, KC_LBRC, KC_RBRC, KC_BSLS,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+        _______, _______, _______, _______, _______,          _______,          _______, _______, KC_SCLN, KC_QUOT, KC_SLSH
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+  ),
+  [_FN3] = LAYOUT(
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_PPLS, KC_PMNS,            KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+           KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_PAST, KC_PSLS,   TG(3),    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+        C(KC_Z), C(KC_X), C(KC_C), C(KC_V),  KC_EQL,            TG(3),          KC_PSLS, KC_COLN, EEP_RST, KC_COMM,  KC_DOT
+    //|-----------------------------------------------------------------------+--------------------------------------------'
+  )
+
+};
