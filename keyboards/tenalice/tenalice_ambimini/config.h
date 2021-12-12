3be1fa3bd7e09fcb2b5a441ef4f6cdc1b4e6fff2
Cerbekos <t.naito1818@gmail.com>
2021-12-13 00:29:25 +0900

    add new file

--- keyboards/tenalice/tenalice_ambimini/config.h
+++ keyboards/tenalice/tenalice_ambimini/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x1042
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Cerbekos
-#define PRODUCT         tenalice_ambimini
+#define PRODUCT         Ambi_MINI
 
 /* key matrix size */
 #define MATRIX_ROWS 6
