8aeaf3b5c23ad39957dbeca3c8ecef1e860ab0b4
Cerbekos <79152714+Cerbekos@users.noreply.github.com>
2023-02-26 13:20:24 +0900

    add new file

--- keyboards/cerbekos/brookten/btkv2/keymaps/cerbekos/keymap.c
+++ keyboards/cerbekos/brookten/btkv2/keymaps/cerbekos/keymap.c
@@ -21,15 +21,18 @@
 #define L3_SPC LT(3,KC_SPC)
 #define S_SLSH MT(MOD_RSFT,KC_SLSH)
 
+#define C_CAPS MT(MOD_LCTL,KC_CAPS)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,        
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SCLN, KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,                  
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,        
+        C_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SCLN, KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS,                  
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  S_SLSH,                  
         KC_LCTL, KC_LGUI, KC_LALT, L3_SPC,  KC_LEFT,  KC_UP,  KC_DOWN, KC_RGHT, L1_ENT,  L2_MENU, KC_RGUI, KC_RCTL,
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PLUS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6, 
         KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_P0,            KC_PDOT,          KC_VOLU, KC_VOLD                          
@@ -135,3 +138,4 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     action_exec((keyevent_t){.key = key, .pressed = false, .time = (timer_read() | 1)});
     return true;
 };
+
