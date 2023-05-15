09183a662a75d5f549a88b55c19b1ce70300f8b6
Cerbekos <79152714+Cerbekos@users.noreply.github.com>
2023-05-15 22:36:16 +0900

    add new file.

---
+++ keyboards/cerbekos/maglit40/config.h
@@ -0,0 +1,29 @@
+// Copyright 2023 cerbekos (@cerbekos)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define MATRIX_MASKED
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 1
+
+#define SPLIT_HAND_PIN GP3
+
+#define SPLIT_MAX_CONNECTION_ERRORS 10
