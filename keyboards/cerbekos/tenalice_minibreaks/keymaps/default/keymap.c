6e2a74ce4acdf0c080b8cb794ff1af2963e39cd5
Cerbekos <79152714+Cerbekos@users.noreply.github.com>
2023-10-22 10:07:45 +0900

    add new Tenalice-minibreaks files.

--- keyboards/cerbekos/tenalice_minibreaks/keymaps/default/keymap.c
+++ keyboards/cerbekos/tenalice_minibreaks/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 #include "keymap_japanese.h"
 
 #define L1_ENT LT(1,KC_ENT)
-#define L2_SCLN LT(2,KC_SCLN)
+#define L2_APP LT(2,KC_APP)
 #define L3_SPC LT(3,KC_SPC)
 #define S_SLSH MT(MOD_RSFT,KC_SLSH)
 
@@ -46,38 +46,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         C_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_ENT,           KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   S_SLSH,  KC_P4,   KC_P5,   KC_P6,
-        KC_LCTL, KC_LGUI,          KC_LALT, L3_SPC,           L1_ENT,  L2_SCLN,                   KC_LEFT, KC_DOWN, KC_RGHT, KC_P1,   KC_P2,   KC_P3,   KC_ENT,
+        KC_LCTL, KC_LGUI,          KC_LALT, L3_SPC,           L1_ENT,  L2_APP,                   KC_LEFT, KC_DOWN, KC_RGHT, KC_P1,   KC_P2,   KC_P3,   L1_ENT, 
                                                     KC_CAPS,                                                                 KC_P0,            TD_DOT
     ),
     [1] = LAYOUT(
-        _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, _______, _______, _______, _______, _______,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,          _______, _______, _______, _______,
-        _______, _______, _______, _______, S_SCLN,  KC_PSLS, KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, _______, _______, _______, _______, _______, _______,
+        _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, _______, KC_CALC, _______, _______, KC_INT3,
+        KC_LBRC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,          KC_ESC,  _______, KC_BSPC, KC_RSFT,
+        _______, _______, _______, _______, S_SCLN,  KC_EQL,  KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, _______, _______, _______, KC_TAB,  KC_EQL,  _______,
         _______, _______,          _______, _______,          _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
                                                     _______,                                                                 _______,          _______
     ),
     [2] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUI, _______, _______, _______, _______, QK_BOOT, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_RMOD,_______, _______,          _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______,          _______, _______,          RGB_TOG, _______,                   _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUI, _______, RGB_M_SW,RGB_M_K, RGB_M_TW,
+        _______, _______,          _______, _______,          RGB_TOG, _______,                   RGB_MOD, RGB_HUD, RGB_RMOD,RGB_M_P, RGB_M_B, RGB_M_R, _______,
                                                      _______,                                                                _______,          _______
     ),
     [3] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, _______, _______, _______, _______, _______,
-        KC_LALT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______,          _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______,          _______, _______,          _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_PGUP, KC_UP,   KC_PGDN, KC_INT3, _______, _______, KC_CALC, _______, _______, KC_INT3,
+        KC_LALT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______,          KC_ESC,  _______, KC_BSPC, KC_RSFT,
+        _______, _______, _______, _______, _______, _______, _______,  KC_END, _______, KC_SCLN, KC_QUOT, KC_UP,   _______, KC_TAB,  KC_EQL,  _______,
+        _______, _______,          _______, _______,          _______, _______,                   KC_HOME, KC_DOWN, KC_END,  _______, _______, _______, _______,
                                                      _______,                                                                _______,          _______
     )
 };
 
 // RGBLayer setting
-const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_RED});
-const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_BLUE});
-const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_GREEN});
-const rgblight_segment_t PROGMEM my_layerCL_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_YELLOW});
-const rgblight_segment_t PROGMEM my_layerNL_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_MAGENTA});
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_RED});
+const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_BLUE});
+const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_GREEN});
+const rgblight_segment_t PROGMEM my_layerCL_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_YELLOW});
+const rgblight_segment_t PROGMEM my_layerNL_layer[] = RGBLIGHT_LAYER_SEGMENTS({2, 1, HSV_MAGENTA});
 
 const rgblight_segment_t * const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_layerNL_layer,
@@ -113,10 +113,9 @@ bool led_update_kb(led_t led_state) {
 // CAPS WORD Indicator
 void caps_word_set_user(bool active) {
     if (active) {
-        rgblight_sethsv_range(HSV_YELLOW, 0, 3);
-
+        rgblight_set_layer_state(1, active);
     } else {
-        rgblight_sethsv_range(HSV_OFF, 0, 3);
+        rgblight_set_layer_state(1, active);
     }
 }
 
@@ -124,8 +123,8 @@ void caps_word_set_user(bool active) {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_INT5, KC_INT4)},
-    [1] = { ENCODER_CCW_CW(KC_VOLU,  KC_VOLD)},
+    [1] = { ENCODER_CCW_CW(RGB_MOD, RGB_RMOD)},
     [2] = { ENCODER_CCW_CW(RGB_HUI, RGB_HUD)},
-    [3] = { ENCODER_CCW_CW(RGB_MOD, RGB_RMOD)},
+    [3] = { ENCODER_CCW_CW(KC_VOLU,  KC_VOLD)},
 };
 #endif
